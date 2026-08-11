cec64364e6270b2bc007819ca37841f27bcc32d2
Chloe Alpert <chloe@chloealpert.com>
2026-08-11 12:35:49 -0400

    remove
    remove

--- newsite/serve.py
+++
@@ -1,9 +0,0 @@
-import os, http.server
-
-os.chdir("/Users/chloealpert/Documents/GitHub/checkia-inc")
-
-http.server.test(
-    HandlerClass=http.server.SimpleHTTPRequestHandler,
-    port=8765,
-    bind="127.0.0.1",
-)
